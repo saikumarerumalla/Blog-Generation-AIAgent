b23b886424a44b97c79801b5a26193dafd443ace
Sai Kumar Erumalla <saikumar.erumalla@gmail.com>
2025-09-06 00:39:49 +0900

    Graph and nodes configured

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -8,6 +8,8 @@ class GraphBuilder:
         self.llm = llm
         self.graph = StateGraph(BlogState)
 
+
+
     def build_topic_graph(self):
         """Builds a graph for generating blog based on the topics."""
 
@@ -22,13 +24,45 @@ class GraphBuilder:
 
         return self.graph
     
+
+    
+    def build_language_graph(self):
+        """Builds a graph for generating blog based on the topics and language."""
+        self.blog_node = BlogNode(self.llm)
+
+        self.graph.add_node("title_creation", self.blog_node.title_creation)
+        self.graph.add_node("content_generation",self.blog_node.content_generation)
+        self.graph.add_node("hindi_translation", lambda state: self.blog_node.translation({**state, "current_language":"Hindi"}))
+        self.graph.add_node("french_translation", lambda state: self.blog_node.translation({**state, "current_language":"French"}))
+        self.graph.add_node("telugu_translation", lambda state: self.blog_node.translation({**state, "current_language":"Telugu"}))
+        self.graph.add_node("route", self.blog_node.route)
+
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", "route")
+        self.graph.add_conditional_edges(
+            "route",
+            self.blog_node.route_decision,
+            {"hindi": "hindi_translation", "french": "french_translation", "telugu": "telugu_translation"}
+            )
+        self.graph.add_edge("hindi_translation", END)
+        self.graph.add_edge("french_translation", END)
+        self.graph.add_edge("telugu_translation", END)
+
+        return self.graph
+    
+
+    
     def setup_graph(self, usecase: str):
         if usecase == "blog":
             self.build_topic_graph()
+        if usecase == "language":
+            self.build_language_graph()
         
         return self.graph.compile()
     
 
 llm =GroqLLM().get_llm()
 graph_builder = GraphBuilder(llm)
-graph= graph_builder.build_topic_graph().compile()
+# graph= graph_builder.build_topic_graph().compile()
+graph= graph_builder.build_language_graph().compile()
--- src/nodes/blog_node.py
+++ src/nodes/blog_node.py
@@ -1,6 +1,6 @@
 from src.states.blogstate import BlogState
-
-
+from langchain_core.messages import SystemMessage, HumanMessage
+from src.states.blogstate import Blog
 
 class BlogNode:
     """A class representing a blog node with a title and content.
@@ -13,7 +13,7 @@ class BlogNode:
         if "topic" in state and state["topic"]:
             prompt="""
                    You are an expert blog content writer. Use Markdown formatting. Generate
-                   a blog title for the {topic}. This title should be creative and SEO friendly and Short.
+                   a blog title for the {topic}. This title should be creative and SEO friendly and Short to around 2 sentences.
                    """
             system_message = prompt.format(topic=state["topic"])
             response = self.llm.invoke(system_message)
@@ -23,8 +23,41 @@ class BlogNode:
         """Generates blog content based on the given title and topic."""
         if "topic" in state and state["topic"]:
             system_prompt = """You are expert blog writer. Use Markdown formatting.
-            Generate a detailed blog content with detailed breakdown for the {topic}"""
+            Generate a detailed and explanatory blog content with detailed breakdown for the {topic}"""
             system_message = system_prompt.format(topic=state["topic"])
             response = self.llm.invoke(system_message)
             return {"blog":{"title": state['blog']['title'], "content":response.content}}
-          
+        
+
+    def translation(self, state:BlogState):
+        """Translates the blog content to the specified Language."""
+
+        translation_prompt = """
+        You are an expert translator. Translate the following blog content to {current_language}.
+        Maintain the original meaning and tone. Adapt cultural references and idioms to be appropriate for {current_language}.
+
+        ORIGINAL CONTENT:
+        {blog_content}
+        """
+
+        blog_content= state['blog']['content']
+        messages = [
+            HumanMessage(translation_prompt.format(current_language=state['current_language'], blog_content=blog_content))
+        ]
+
+        translated_content = self.llm.with_structured_output(Blog).invoke(messages)
+        return {"blog":{"title": state['blog']['title'], "content":translated_content.content}}
+
+    def route(self, state:BlogState):
+        return {"current_language": state["current_language"]}
+    
+    def route_decision(self, state:BlogState):
+        """Route the content to the respective language translation."""
+        if state["current_language"]=="Hindi":
+            return "hindi"
+        elif state["current_language"]=="French":
+            return "french"
+        elif state["current_language"]=="Telugu":
+            return "telugu"
+        else:
+            return state["current_language"]
